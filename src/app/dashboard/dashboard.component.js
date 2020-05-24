29711edc53eb70bb3d4ab8a194a6f78890a1ea6c
vikasmagar512@gmail.com <--add>
2020-05-24 18:19:47 +0530

    basic functionality working

--- src/app/dashboard/dashboard.component.js
+++ src/app/dashboard/dashboard.component.js
@@ -1,3 +1,4 @@
+var jq = $.noConflict();
 /**
  * Dashboard component
  * @class dashboard
@@ -25,9 +26,6 @@
       // function error(response) { /* handle error */ });
      
         vm.schema ={
-          // "$schema": "http://json-schema.org/draft-04/schema#",
-          // "title": "Asset",
-          // "description": "An Asset Json",
           "type": "object",
           "properties": {
             "uri": {
@@ -107,75 +105,7 @@
           },
           // "required": ['uri']
         }
-        vm.schema2={
-          // "$schema": "http://json-schema.org/draft-04/schema#",
-          // "title": "Asset",
-          // "description": "An Asset Json",
-          // "type": "object",
-          // "properties": {
-            // "parameters": {
-              "type": "object",
-              "properties": {
-                "type": "object",
-                "ORE_BRIDGE_0_Temperature_C": {
-                  "type": "object",
-                  "properties": {
-                    "pinType": {
-                      "type": "string",
-                      "title": "Pin Type",
-                      "enum": [ "analog", "Digital"]
-                    },
-                    "ignoreTile": {
-                      "description": "The unique identifier for a product",
-                      "type": "boolean"
-                    },
-                    "tagType": {
-                      "description": "Name of the product",
-                      "type": "string"
-                    },
-                    "tag": {
-                      "description": "Name of the product",
-                      "type": "string"
-                    },
-                    "uom": {
-                      "description": "Name of the product",
-                      "type": "string"
-                    },
-                    "binary": {
-                      "description": "The unique identifier for a product",
-                      "type": "boolean"
-                    },
-                    "multiplier": {
-                      "description": "The unique identifier for a product",
-                      "type": "integer"
-                    },
-                    "low": {
-                      "description": "The unique identifier for a product",
-                      "type": "integer"
-                    },
-                    "high": {
-                      "description": "The unique identifier for a product",
-                      "type": "integer"
-                    },
-                    "decimal": {
-                      "description": "The unique identifier for a product",
-                      "type": "boolean"
-                    },
-                    "tileType": {
-                        "type": "string",
-                        "title": "TileType",
-                        "enum": [ "text", "gauge"]
-                      },
-                    },
-                    "index": {
-                      "description": "The unique identifier for a product",
-                      "type": "integer"
-                    },
-                  }
-                }
-          //   }
-          // }
-        }
+       
         vm.uiSchema ={
           "type": "VerticalLayout",
           "elements": [
@@ -293,7 +223,7 @@
             }
           ],
         }
-        vm.data2 ={
+        vm.data2 = {
           // "parameters": {
             "ORE_BRIDGE_0_Temperature_C": {
               "pinType": "analog",
@@ -309,14 +239,275 @@
               "tileType": "gauge",
               "index": 1
             },
+            "ORE_BRIDGE_1_Temperature_C": {
+              "pinType": "analog",
+              "ignoreTile": false,
+              "tagType": "West_Trolley_Blower_Temp",
+              "tag": "ORE_BRIDGE_1_Temperature_C",
+              "uom": "C",
+              "binary": "false",
+              "multiplier": "1",
+              "low": 0,
+              "high": 60,
+              "decimal": false,
+              "tileType": "gauge",
+              "index": 2
+            },
+            "ORE_BRIDGE_2_Temperature_C": {
+              "pinType": "analog",
+              "ignoreTile": false,
+              "tagType": "West_Trolley_G-Box_Temp",
+              "tag": "ORE_BRIDGE_2_Temperature_C",
+              "uom": "C",
+              "binary": "false",
+              "multiplier": "1",
+              "low": 0,
+              "high": 60,
+              "decimal": false,
+              "tileType": "gauge",
+              "index": 3
+            },
+            
           // }
         }
+        vm.schema2={
+          // "$schema": "http://json-schema.org/draft-04/schema#",
+          // "title": "Asset",
+          // "description": "An Asset Json",
+          // "type": "object",
+          // "properties": {
+            // "parameters": {
+            "type": "object",
+            "properties": {
 
+              // "pinType": {
+              //   "type": "string",
+              //   "title": "Pin Type",
+              //   "enum": [ "analog", "Digital"]
+              // },
+              // "ignoreTile": {
+              //   "description": "The unique identifier for a product",
+              //   "type": "boolean"
+              // },
+              // "tagType": {
+              //   "description": "Name of the product",
+              //   "type": "string"
+              // },
+              // "tag": {
+              //   "description": "Name of the product",
+              //   "type": "string"
+              // },
+              // "uom": {
+              //   "description": "Name of the product",
+              //   "type": "string"
+              // },
+              // "binary": {
+              //   "description": "The unique identifier for a product",
+              //   "type": "boolean"
+              // },
+              // "multiplier": {
+              //   "description": "The unique identifier for a product",
+              //   "type": "integer"
+              // },
+              // "low": {
+              //   "description": "The unique identifier for a product",
+              //   "type": "integer"
+              // },
+              // "high": {
+              //   "description": "The unique identifier for a product",
+              //   "type": "integer"
+              // },
+              // "decimal": {
+              //   "description": "The unique identifier for a product",
+              //   "type": "boolean"
+              // },
+              // "tileType": {
+              //     "type": "string",
+              //     "title": "TileType",
+              //     "enum": [ "text", "gauge"]
+              //   },
+              //   "index": {
+              //     "description": "The unique identifier for a product",
+              //     "type": "integer"
+              //   },
+
+            // "type": "object",
+            // "ORE_BRIDGE_0_Temperature_C": {
+            // }
+          }
+        }
+
+        Object.keys(vm.data2).map(key=>{
+          vm.schema2.properties[key] = {
+            "type": "object",
+            "properties": {
+              // "pinType": {
+              //   "type": "string",
+              //   "title": "Pin Type",
+              //   "enum": [ "analog", "Digital"]
+              // },
+              "ignoreTile": {
+                "description": "The unique identifier for a product",
+                "type": "boolean"
+              },
+              "tagType": {
+                "description": "Name of the product",
+                "type": "string"
+              },
+              // "tag": {
+              //   "description": "Name of the product",
+              //   "type": "string"
+              // },
+              // "uom": {
+              //   "description": "Name of the product",
+              //   "type": "string"
+              // },
+              // "binary": {
+              //   "description": "The unique identifier for a product",
+              //   "type": "boolean"
+              // },
+              // "multiplier": {
+              //   "description": "The unique identifier for a product",
+              //   "type": "integer"
+              // },
+              // "low": {
+              //   "description": "The unique identifier for a product",
+              //   "type": "integer"
+              // },
+              // "high": {
+              //   "description": "The unique identifier for a product",
+              //   "type": "integer"
+              // },
+              // "decimal": {
+              //   "description": "The unique identifier for a product",
+              //   "type": "boolean"
+              // },
+              // "tileType": {
+              //   "type": "string",
+              //   "title": "TileType",
+              //   "enum": [ "text", "gauge"]
+              // },
+              // "index": {
+              //   "description": "The unique identifier for a product",
+              //   "type": "integer"
+              // },
+            },
+          }
+        })
+
+        vm.uiSchema2 ={
+          "type": "VerticalLayout",
+          "elements": [
+            // {
+              // "type": "Group",
+              // "label": "Attribute",
+              // "elements":[
+              //   {
+              //     "type": "VerticalLayout",
+              //     "elements": [
+              //         {
+              //           "type": "HorizontalLayout",  
+              //           "elements": [
+                          // {
+                          //   "type": "Control",
+                          //   "scope": { "$ref": "#/properties/pinType" }
+                          // },
+                          {
+                              "type": "Control",
+                              "scope": { "$ref": "#/properties/ignoreTile" }
+                          },
+                          {
+                            "type": "Control",
+                            "scope": { "$ref": "#/properties/tagType" }
+                          },
+                      //   ]
+                      // },
+                      // {
+                      // "type": "HorizontalLayout",
+                      // "elements": [
+                      //     {
+                      //       "type": "Control",
+                      //       "scope": { "$ref": "#/properties/tag" }
+                      //     },{
+                      //       "type": "Control",
+                      //       "scope": { "$ref": "#/properties/uom" }
+                      //     },{
+                      //       "type": "Control",
+                      //       "scope": { "$ref": "#/properties/binary" }
+                      //     },
+                      //   ]
+                      // },
+                      // {
+                      //   "type": "HorizontalLayout",
+                      //   "elements": [ 
+                      //     {
+                      //       "type": "Control",
+                      //       "scope": { "$ref": "#/properties/multiplier" }
+                      //     },{
+                      //       "type": "Control",
+                      //       "scope": { "$ref": "#/properties/low" }
+                      //     },
+                      //     {
+                      //       "type": "Control",
+                      //       "scope": { "$ref": "#/properties/high" }
+                      //     },
+                      //   ]
+                      // }, 
+                      // {
+                      //   "type": "HorizontalLayout",
+                      //   "elements": [ 
+                      //     {
+                      //       "type": "Control",
+                      //       "scope": { "$ref": "#/properties/decimal" }
+                      //     },{
+                      //       "type": "Control",
+                      //       "scope": { "$ref": "#/properties/tileType" }
+                      //     },
+                      //     {
+                      //       "type": "Control",
+                      //       "scope": { "$ref": "#/properties/index" }
+                      //     },
+                      //   ]
+                      // }, 
+            //       ]
+            //     }
+            //   ]
+            // },
+          ]
+        }
+
+        function myFunction(e) {
+          alert('myFunction')
+          let temp_data2 = {}
+          let temp_schema2 = vm.schema2
+          let newKey = document.getElementById("#/properties/id").value
+          const keys = Object.keys(vm.data2)
+          debugger
+          console.log('vm.data2 ',vm.data2)
+          keys.map((oldKey,i)=>{
+            temp_data2[newKey + oldKey.slice(vm.oldAsset_ID_value.length)] = {...vm.data2[oldKey], tag: newKey  + oldKey.slice(vm.oldAsset_ID_value.length)}
+            temp_schema2.properties[newKey + oldKey.slice(vm.oldAsset_ID_value.length)] = vm.schema2.properties[oldKey]
+            
+            delete vm.schema2.properties[oldKey]
+            delete temp_schema2.properties[oldKey]
+            delete vm.data2.oldKey
+          })
+          vm.data2 = temp_data2;
+          vm.schema2 = temp_schema2;
+        }
+        
+        vm.changeData=function(event){
+          alert('changeData')
+        }
         vm.sensorDisplay = function(sensor) {
           $location.path('/asset/' + sensor.id);
         };
 
         vm.$onInit = function() {
+          setTimeout(()=>{
+            vm.oldAsset_ID_value = document.getElementById("#/properties/id").value
+            document.getElementById("#/properties/id").addEventListener("change", myFunction);
+          },1000)
           sensors = dashboardService.getSensorList();
           sensors.promise.then(function(sensors) {
             vm.sensorList = sensors;
